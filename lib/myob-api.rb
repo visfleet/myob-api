de4c5b4c59dc51ea36d619b6e5983a62150062ef
Pat Allan <pat@freelancing-gods.com>
2016-01-22 15:37:55 +1100

    Add service invoices as a known model.
    As documented here: http://developer.myob.com/api/accountright/v2/sale/invoice/invoice_service/

--- lib/myob-api.rb
+++ lib/myob-api.rb
@@ -23,6 +23,7 @@ require 'myob/api/models/invoice'
 require 'myob/api/models/invoice_item'
 require 'myob/api/models/order'
 require 'myob/api/models/order_item'
+require 'myob/api/models/service_invoice'
 
 require 'myob/api/models/payroll_category'
 require 'myob/api/models/wage'
